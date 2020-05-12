1417cde83f93961023c46654c2b2fbdb0bf460c2
matjol <matiasj@gmx.de>
2020-05-12 13:51:24 +0200

    Projektdateien hinzufügen.

---
+++ Ta-Re-it2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ta_Re_it2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bitte gib den ersten Summanden ein: ");
+            string ersterSummand = Console.ReadLine();
+            Console.Write("Bitte gib den zweiten Summanden ein: ");
+            string zweiterSummand = Console.ReadLine();
+
+            // Wandel Text in Gleikommazahlen
+            double ersterSummandAlsZahl = Convert.ToDouble(ersterSummand);
+            double zweiterSummandAlsZahl = Convert.ToDouble(zweiterSummand);
+
+            // Berechnung ausführen
+            double summe = ersterSummandAlsZahl + zweiterSummandAlsZahl;
+
+            // Ausgabe
+            Console.WriteLine("Die Summe ist: {0}", summe);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
